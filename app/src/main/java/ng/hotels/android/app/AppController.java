e5d6f518af9234c931ede438aca1410b24c3f996
Nsikaktopdown <nsikakthompson73@gmail.com>
2017-09-27 22:45:21 +0100

    Added font utils and files

---
+++ app/src/main/java/ng/hotels/android/app/AppController.java
@@ -0,0 +1,24 @@
+package ng.hotels.android.app;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by Nsikak on 9/27/17.
+ */
+
+public class AppController  extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //intialized default font
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Lato-Bold.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+
+    }
+}
--- app/src/main/java/ng/hotels/android/app/MainActivity.java
+++ app/src/main/java/ng/hotels/android/app/MainActivity.java
@@ -1,15 +1,24 @@
 package ng.hotels.android.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
