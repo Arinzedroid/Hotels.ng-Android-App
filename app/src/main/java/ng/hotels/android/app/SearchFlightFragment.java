0b72a61f4f86f6a7cd0e787a71de8add2afbd423
Idorenyin Obong <idee4ril@gmail.com>
2017-09-28 00:44:34 +0100

    added SearchFlight UI

---
+++ app/src/main/java/ng/hotels/android/app/SearchFlightFragment.java
@@ -0,0 +1,34 @@
+package ng.hotels.android.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchFlightFragment extends Fragment {
+
+
+
+
+    public SearchFlightFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search_flight, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+}
