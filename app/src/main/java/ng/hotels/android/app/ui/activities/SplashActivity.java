1af5465450c03ace5aaad5c64bce828d91dc8913
Idorenyin Obong <idee4ril@gmail.com>
2017-09-30 23:25:58 +0100

    added on-boarding activity

--- app/src/main/java/ng/hotels/android/app/ui/activities/SplashActivity.java
+++ app/src/main/java/ng/hotels/android/app/ui/activities/SplashActivity.java
@@ -5,17 +5,28 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import ng.hotels.android.app.utils.PreferenceUtils;
+
 /**
  * Created by tody on 9/28/17.
  */
 
 public class SplashActivity extends AppCompatActivity {
+
+    Intent intent;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this,MainActivity.class);
+        if (PreferenceUtils.isThisUserFirstTime(this))
+            intent = new Intent(this, OnBoardingActivity.class);
+        else
+            intent = new Intent(this, MainActivity.class);
+
         startActivity(intent);
         finish();
+
     }
+
 }
---
+++ app/src/main/java/ng/hotels/android/app/ui/fragments/OnBoardingFragment.java
@@ -0,0 +1,62 @@
+package ng.hotels.android.app.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ng.hotels.android.app.R;
+
+public class OnBoardingFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    @BindView(R.id.image_on_boarding)
+    ImageView imageOnBoarding;
+
+    @BindView(R.id.text_detail_on_boarding)
+    TextView onBoardingTextDetail;
+
+    int[] bgs = new int[]{R.drawable.ic_flight_black, R.drawable.ic_tv_black_24dp, R.drawable.ic_lightbulb_outline_black_24dp};
+
+    public OnBoardingFragment() {
+        // Required empty public constructor
+    }
+
+    public static OnBoardingFragment newInstance(int sectionNumber) {
+        OnBoardingFragment fragment = new OnBoardingFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_onboarding, container, false);
+        ButterKnife.bind(this,view);
+
+        int page = getArguments().getInt(ARG_SECTION_NUMBER);
+
+        if (page==1){
+            onBoardingTextDetail.setText(R.string.hotel_booking);
+        } else if (page==2){
+            onBoardingTextDetail.setText(R.string.flight_booking);
+        } else if (page==3){
+            onBoardingTextDetail.setText(R.string.better_prices);
+        }
+
+        imageOnBoarding.setBackgroundResource(bgs[getArguments().getInt(ARG_SECTION_NUMBER) - 1]);
+
+        return view;
+    }
+
+
+}
