0b9dde452cc7e38d7a3e3bb1525be836bdb7ebb0
lanre01 <olanre12waju@gmail.com>
2017-10-04 22:57:32 +0100

    Add multidex support for older devices

--- app/src/main/java/ng/hotels/android/app/ui/fragments/LoginFragment.java
+++ app/src/main/java/ng/hotels/android/app/ui/fragments/LoginFragment.java
@@ -129,14 +129,19 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        /*if (context instanceof OnFragmentInteractionListener){
+        if (context instanceof OnFragmentInteractionListener){
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + "must implement OnFragmentInteractionListener");
-        }*/
+        }
     }
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 
     @OnClick(R.id.text_forgot_password)
     public void clickForgotPassword() {
