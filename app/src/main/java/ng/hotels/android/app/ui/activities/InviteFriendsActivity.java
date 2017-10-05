2637ff09a9f1703e462ed3bd017dce7f5637cabc
lanre01 <olanre12waju@gmail.com>
2017-10-05 18:58:59 +0100

    Add Invite Friends Activity

---
+++ app/src/main/java/ng/hotels/android/app/ui/activities/InviteFriendsActivity.java
@@ -0,0 +1,15 @@
+package ng.hotels.android.app.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ng.hotels.android.app.R;
+
+public class InviteFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_friends);
+    }
+}
