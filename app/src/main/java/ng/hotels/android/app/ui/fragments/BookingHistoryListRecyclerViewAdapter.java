fc3f8a967d9bbdc57704c3bd516c5ff80af14146
lanre01 <olanre12waju@gmail.com>
2017-10-05 22:12:24 +0100

    Add base code for booking details page

--- app/src/main/java/ng/hotels/android/app/ui/fragments/BookingHistoryListRecyclerViewAdapter.java
+++ app/src/main/java/ng/hotels/android/app/ui/fragments/BookingHistoryListRecyclerViewAdapter.java
@@ -6,10 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
+import ng.hotels.android.app.R;
 import ng.hotels.android.app.ui.fragments.BookingHistoryListFragment.OnListFragmentInteractionListener;
 import ng.hotels.android.app.ui.fragments.dummy.DummyContent.DummyItem;
-
-import java.util.List;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
