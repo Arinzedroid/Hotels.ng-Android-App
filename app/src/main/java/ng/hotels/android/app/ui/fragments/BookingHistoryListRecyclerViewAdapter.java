857aac657a8cac9d66c6bede54c0179d9a470036
lanre01 <olanre12waju@gmail.com>
2017-10-06 12:03:18 +0100

    Add xml code for booking history listing page

--- app/src/main/java/ng/hotels/android/app/ui/fragments/BookingHistoryListRecyclerViewAdapter.java
+++ app/src/main/java/ng/hotels/android/app/ui/fragments/BookingHistoryListRecyclerViewAdapter.java
@@ -30,7 +30,7 @@ public class BookingHistoryListRecyclerViewAdapter extends RecyclerView.Adapter<
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_booking_history, parent, false);
+                .inflate(R.layout.content_booking_history, parent, false);
         return new ViewHolder(view);
     }
 
