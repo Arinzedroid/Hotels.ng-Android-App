66c5be8afbab46a0cddea39654c883ae2e4916d3
lanre01 <olanre12waju@gmail.com>
2017-10-06 20:12:48 +0100

    Optimized Invite friends activity and booking history activity

--- app/src/main/java/ng/hotels/android/app/ui/fragments/LoyaltyCoin.java
+++ app/src/main/java/ng/hotels/android/app/ui/fragments/LoyaltyCoin.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ng.hotels.app.R;
+import ng.hotels.android.app.R;
 
 /**
  * A simple {@link Fragment} subclass.
